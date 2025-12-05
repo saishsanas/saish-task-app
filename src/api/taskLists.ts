d1c45b5686525a45929a1fd2b7a590afd6b07946
Saish Sanas <saishsanas@gmail.com>
2025-12-05 17:50:30 +0530

    Update API URL to Railway

--- src/api/taskLists.ts
+++ src/api/taskLists.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import TaskList from "../domain/TaskList";
 
-const API_BASE_URL = "http://localhost:8080/api/task-lists";
+
+const API_BASE_URL = "https://saish-task-backend-production.up.railway.app/api/task-lists";
 
 export const fetchTaskLists = () => axios.get(API_BASE_URL);
 
